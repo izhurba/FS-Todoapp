7db6d5c1f55cb0c7eb94ebd106c41d53c5b5ba9d
Ilya Zhurba <i.n.zhurba@gmail.com>
2022-02-16 12:41:42 -0700

    Changed code to new initial state

--- app.py
+++ app.py
@@ -5,22 +5,30 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
 
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-    description = request.data.get('description')
+app = Flask(__name__)
+@app.route('/')
+def index():
+  return render_template('index.html', data=[{
+    'description': 'Todo 1'
+  }, {
+    'description': 'Todo 2'
+  }, {
+    'description': 'Todo 3'
+  }])
+
 
 if __name__ == '__main__':
     app.debug = True
