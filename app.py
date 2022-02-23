08961ba8652a686e6704823f3fa09cea45f27d7f
Ilya Zhurba <i.n.zhurba@gmail.com>
2022-02-22 17:07:25 -0700

    Added todolists that hold todo items, as well as the model view of them

--- app.py
+++ app.py
@@ -15,11 +15,21 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolist.id'), nullable=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+class Todolist(db.Model):
+  __tablename__ = 'todolist'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+
+  def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -65,11 +75,17 @@ def delete_todo(todo_id):
   finally:
     db.session.close()
     return jsonify({ 'success': True })
+    
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+  return render_template('index.html', lists = Todolist.query.all(), 
+  active_list = Todolist.query.get(list_id),
+   todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.order_by('id').all())
-
+  return redirect(url_for('get_list_todos', list_id=1))
 
 if __name__ == '__main__':
     app.debug = True
